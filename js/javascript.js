c80e1f521ef8f5e9571a61240e5640b21a04134c
Ravalash <13k0873juhky@opayq.net>
2020-04-11 17:26:34 -0400

    Cleaning up code, updating Readme

--- js/javascript.js
+++ js/javascript.js
@@ -1,24 +1,27 @@
-var tasteDiveBaseQueryUrl = "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?type=movies&info=1&k=362316-MovieMe-NN3BYWU6&q=movie%3A"
-var omdbBaseQueryUrl = "https://www.omdbapi.com/?apikey=14427a54&t="
+// Initialize global variables
 var returnedMovieArray = [];
 var contentMovieChoice = [];
 var savedMovieArray = [];
 var savedMovieHolder="";
-//Keybind for enter key in search box
+var videoSearchResults = [];
 
+//Keybind for enter key in search box. Tests whether enter key was pressed and search box has data.
 $('#search').keypress(function (event) {
     if (event.keyCode == 13 && $("#search").val() !="" ) {
         event.preventDefault();
         M.toast({html: 'Working on it!', displayLength: 15000	});
         sessionStorage.removeItem("movieMeMovieArray")
         var movie = $.trim($("#search").val());
+        //Caps saved movies at 6. Stores last value in case nonsense was typed.
         while (savedMovieArray.length>5) {
             savedMovieHolder = savedMovieArray[5];
             savedMovieArray.pop();
         }
-         savedMovieArray.unshift(movie);
+        savedMovieArray.unshift(movie);
+        // Saves to session storage for use on other pages
         sessionStorage.setItem('movieMeSavedMovieArray', JSON.stringify(savedMovieArray));
         var movieName="";
+        // Replaces spaces in movie title with HTML code
         for(i=0;i<movie.length;i++){
             if (movie[i] == ' '){
                 movieName = movieName+ '%20';
@@ -27,32 +30,29 @@ $('#search').keypress(function (event) {
             movieName = movieName+movie[i];
             }
         }
+        var tasteDiveBaseQueryUrl = "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?type=movies&info=1&k=362316-MovieMe-NN3BYWU6&q=movie%3A"
         var tasteDiveQueryUrl = tasteDiveBaseQueryUrl + movieName;
-        for(i=0;i<4;i++) {
+        // Checks to see if saved searches are checked off and adds them as additional parameters
+        for(i=0;i<6;i++) {
             var additionalSearchName = "";
             if ($('#savedSearches input').eq(i)[0].checked == true){
                 var savedSearchName = $('#savedSearches input ~ span').eq(i).text();
                 for(x=0;x<savedSearchName.length;x++){
                     if (savedSearchName.charCodeAt(x)==32) {
-                        additionalSearchName = additionalSearchName + '%20'
+                        additionalSearchName = additionalSearchName + '%20';
                     }
                     else {
                         additionalSearchName = additionalSearchName + savedSearchName[x];
                     }
                 }
                 tasteDiveQueryUrl = tasteDiveQueryUrl+'%2Cmovie%3A'+additionalSearchName;
-            }
-                
+            }          
         }
-        console.log(tasteDiveQueryUrl);
         apiCall(tasteDiveQueryUrl, buildReturnedMovies);
-
     }
 })
 
-
-//Loads content once the movies page loads.
-
+//Initializes pages on load. Gathers data from session storage, hides and displays elements
 $(window).on('load', function () {
     $('.modal').modal();
     M.Toast.dismissAll();
@@ -69,7 +69,6 @@ $(window).on('load', function () {
         }
     }
     else if (window.document.title == "MovieMe - Homepage") {
-        // returnedMovieArray = [];
         savedMovieArray = JSON.parse(sessionStorage.getItem('movieMeSavedMovieArray'));
         if (savedMovieArray == null){
             savedMovieArray = [];
@@ -78,7 +77,6 @@ $(window).on('load', function () {
             for (i=0;i<savedMovieArray.length;i++) {
                 $('#savedSearches').find('span').eq(i).text(savedMovieArray[i]);
                 $('#savedSearches .col').eq(i).attr('style', 'display:block');
-
             }
         }
     }
@@ -92,28 +90,18 @@ $(window).on('load', function () {
         else {
             $("#no-content").attr("style", "display:block");
             $("#content").attr("style", "display:none");
-
         }
     }
 })
 
-
-
-
-//Event handler to capture search field from input box.
-$("#search-movie").on("click", function (movieSearch) {
-    movieSearch.preventDefault();
-
-    var movie = $.trim($("#movie-request").val());
-})
-
-
 //Loop to cycle through Taste Dive array and perform OMDB call for each item. Places each item into a returned movies array
 function buildReturnedMovies(response) {
     returnedMovies = response;
-    if(returnedMovies.Similar.Results.length < 8){
+    // Checks for returned results. requires minimum of 12 in case some are invalid from OMDB
+    if(returnedMovies.Similar.Results.length < 12){
         M.Toast.dismissAll();
         M.toast({html: 'No Results Found'});
+        // Trims unfound result from saved searches. Replaces with removed value from beginning
         savedMovieArray.shift();
         if (savedMovieArray.length >= 4 && savedMovieHolder != null) {
         savedMovieArray.push(savedMovieHolder);
@@ -122,9 +110,9 @@ function buildReturnedMovies(response) {
         return;
     }
     for (i = 0; i < returnedMovies.Similar.Results.length; i++) {
-
         var formattedMovieName = "";
         var returnedMovieTitle = returnedMovies.Similar.Results[i].Name;
+        // Encodes returned characters as HTML codes for query
         for(x=0;x<returnedMovieTitle.length;x++) {
             if (returnedMovieTitle.charCodeAt(x) == 8211) {
                 formattedMovieName = formattedMovieName + '%2D';
@@ -145,14 +133,13 @@ function buildReturnedMovies(response) {
             formattedMovieName = formattedMovieName + returnedMovieTitle[x];
             }
         }
+        var omdbBaseQueryUrl = "https://www.omdbapi.com/?apikey=14427a54&t="
         var omdbQueryUrl = omdbBaseQueryUrl + formattedMovieName;
-        console.log(omdbQueryUrl);
-        apiCall(omdbQueryUrl, buildMovieArray);
-      
+        apiCall(omdbQueryUrl, buildMovieArray);  
     }
 }
 
-
+// Adds OMDB objects into array for later reference
 function buildMovieArray(response) {
     returnedMovie = response;
     if (returnedMovie.Response != 'False') {
@@ -163,6 +150,7 @@ function buildMovieArray(response) {
         window.location = "movies.html"
     }
 }
+
 //Generates search results page based on poster and title
 function loadSearchResults() {
     for (i = 0; i < 8; i++) {
@@ -175,43 +163,67 @@ function loadSearchResults() {
     $('#content img').on('click', function () {
         event.preventDefault();
         loadContentPage(event);
-
     })
 }
 
-
-
-//Event handler for search results click
-
-
-//Generate content page from search results click
+//Navigates to content page once a movie poster is clicked
 function loadContentPage(event) {
-    test = event;
     var posterChoice = event.target.getAttribute('data');
     var movieChoice = returnedMovieArray[posterChoice];
     sessionStorage.setItem('movieMeMovieChoice', JSON.stringify(movieChoice));
     window.location = "content.html"
 }
 
+//Dynamically generates information on the content page
 function loadMovieContent() {
     $('#content').find('.poster').attr('src', contentMovieChoice.Poster);
-
     $('#content').find('h4').text(contentMovieChoice.Title);
     $('#content').find('.rated').text("Rated: " + contentMovieChoice.Rated);
     $('#content').find('.released').text("Released: " + contentMovieChoice.Released);
     $('#content').find('.runtime').text("Runtime: " + contentMovieChoice.Runtime);
     $('#content').find('.genre').text("Genre: " + contentMovieChoice.Genre);
     $('#content').find('.director').text("Director: " + contentMovieChoice.Director);
-    $('#content').find('.description').text("Plot: " + contentMovieChoice.Plot);
-        
-    for (i = 0; i < 3; i++) {
-
+    $('#content').find('.description').text("Plot: " + contentMovieChoice.Plot);       
+    for (i = 0; i < contentMovieChoice.Ratings.length; i++) {
         $("#ratings-list").find("li").eq(i).text(contentMovieChoice.Ratings[i].Source + ": " + contentMovieChoice.Ratings[i].Value);
-
     }
+    var movieStored = $.parseJSON(sessionStorage.getItem("movieMeMovieChoice"))
+    var movieSelected = movieStored.Title
+    var releaseYear = parseInt(movieStored.Year)
+    var tmdbMovieSearchURL = "https://api.themoviedb.org/3/search/movie?api_key=8906f66d9af7f1f04b7db42d2121078c&language=en-US&query=" + movieSelected + "&page=1&include_adult=false&year=" + releaseYear;
+    apiCall(tmdbMovieSearchURL, buildMovieId)
+}   
+
+//Extracts movie ID from TMDB call.
+function buildMovieId(response) {
+    var movieID = response.results[0].id;
+    var tmdbVideoSearchURL = "https://api.themoviedb.org/3/movie/" + movieID + "/videos?api_key=8906f66d9af7f1f04b7db42d2121078c&language=en-US";
+    apiCall(tmdbVideoSearchURL, buildVideos);
 }
 
-
+//Extracts all returned videos from TMDB call and builds containers for videos on content page
+function buildVideos(response) {
+    for (let i = 0; i < response.results.length; i++) {
+        const videoKey = response.results[i].key;
+        var youtubeUrl = "https://www.youtube.com/embed/" + videoKey;
+        videoSearchResults.push(youtubeUrl);
+        //Adds containers to hold responsive videos
+        var videoDiv = $("<div class= 'video-container'>")
+        videoDiv.attr("id", "youtube-videos")
+        //Adds iframe divs for every youtube video found
+        var iframeDiv = $("<iframe />");
+        iframeDiv.attr("id", "iframe-container")
+        iframeDiv.attr("src", youtubeUrl);
+        iframeDiv.attr("width", 400)
+        iframeDiv.attr("height", 315)
+        iframeDiv.attr("frameborder", 0)
+        iframeDiv.attr("allowfullscreen", "")
+        videoDiv.append(iframeDiv)
+        //Pushes vidoes to HTML File
+        $("#videos-container").prepend("<br>");
+        $("#videos-container").prepend(videoDiv);
+    }    
+}
 
 //Makes an API call and then calls the function in the second argument. Requires a URL as the first argument and function as the second
 function apiCall(apiQuery, apiFunction) {
@@ -220,5 +232,3 @@ function apiCall(apiQuery, apiFunction) {
         method: "GET"
     }).then(apiFunction)
 }
-
-
